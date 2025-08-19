5cab9464037ffe957bb7de121a92132c3c502d48
DBVJDIV <2321050022@student.humg.edu.vn>
2025-08-19 23:06:32 +0700

    done_11 19/8

---
+++ ecom/category/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'slug')
+    list_filter = ('category_name',)
+    search_fields = ('category_name', 'slug')
+    ordering = ('-id',)
+admin.site.register(Category)
